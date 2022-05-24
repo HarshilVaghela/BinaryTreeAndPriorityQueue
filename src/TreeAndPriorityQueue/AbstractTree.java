727cb386f5ddb80fe5dce3bf36837d2158fecd05
HPP <HPP@DESKTOP-8CTDDK4>
2022-05-24 13:28:41 -0500

    initial commmit

--- src/TreeAndPriorityQueue/AbstractTree.java
+++ src/TreeAndPriorityQueue/AbstractTree.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TreesAndPriorityQueue;
+package TreeAndPriorityQueue;
 
 import java.util.Iterator;
 import java.util.ArrayList;
